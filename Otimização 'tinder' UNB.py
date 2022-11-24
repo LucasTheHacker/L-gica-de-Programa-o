8aff78e368d832234f562ea142d07faa59443746
LucasTheHacker <106922188+LucasTheHacker@users.noreply.github.com>
2022-11-24 20:20:53 -0300

    Update and rename Sistema de avaliações de aplicativo de namoro UNB.py to Otimização 'tinder' UNB.py

--- Otimização 'tinder' UNB.py
+++ Otimização 'tinder' UNB.py
@@ -1,6 +1,8 @@
 '''D ≤ 100   : ''É o amor da minha vida!"
 100 < D ≤ 200 : "Talvez dê certo"
 D > 200 : "Não vale a pena investir'''
+#A entrada consiste em 4 linhas,sendo as duas primeiras (x1,y1) a localização do usuário e as duas últimas (x2,y2) a localização de sua mais nova paquera.
+
 
 import math
 x1 = int(input())

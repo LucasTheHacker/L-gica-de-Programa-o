b355ecb818b9dee0346349199b7ffbf431009dec
LucasTheHacker <106922188+LucasTheHacker@users.noreply.github.com>
2022-11-25 10:16:30 -0300

    Add files via upload

---
+++ Debugando Estruturas de Decisão UNB.py
@@ -0,0 +1,35 @@
+'''Nesse desafio, chamado 'Debugando Estruturas de Decisão', devemos avaliar os requisitos
+do programa e corrigir um código mal escrito para entregar os
+índices de diversos tipos a uma empresa'''
+
+indiceReclamacao = int(input())
+percentReclamResolPrim = int(input())
+percentCliCancel = int(input())
+indiceIndisponibilidade = int(input())
+canceladoPorProblema = int(input())
+
+if (percentReclamResolPrim >= 60 and percentReclamResolPrim <=100):
+    indice = indiceReclamacao - 5
+elif percentReclamResolPrim > 100:
+    indice = indiceReclamacao + 100
+else:
+    indice = indiceReclamacao + 15
+print(f'{indice}')
+
+if ( percentCliCancel >=10):
+    if (canceladoPorProblema==1):
+        indice = indice + 80
+    else:
+        indice = indice - 30
+elif percentCliCancel < 10:
+    if (canceladoPorProblema==1):
+        indice = indice + 50
+    else:
+        indice = indice - 10
+print(f'{indice}');
+
+if (indiceIndisponibilidade >= 10):
+    indice = indice + 70
+else:
+    indice = indice - 20
+print(f'{indice}')

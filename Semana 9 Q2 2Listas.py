bfd68c920d18527fbd9940855ced995d0b1f208b
LucasTheHacker <106922188+LucasTheHacker@users.noreply.github.com>
2023-01-09 16:55:11 -0300

    Subtração de Listas Otimizado

--- Semana 9 Q2 2Listas.py
+++ Semana 9 Q2 2Listas.py
@@ -4,13 +4,9 @@ crewmates = []
 for i in range(num_inputs):
     nome_de_usuario = input()
     usernames.append(nome_de_usuario)
-if num_inputs == 1:
-    print(*usernames) #printa somente a string que entrou
 else:
     impostores = input().split(sep=' ')
     for i in range(len(usernames)):
         if usernames[i] not in impostores:  #se o username não estiver em impostores, adiciona a crewmates
             crewmates.append(usernames[i])          
     print(*crewmates)
-        
-    
